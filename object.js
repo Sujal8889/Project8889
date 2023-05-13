611904e1d32e8de532f7e761775390e1e9c39fd8
Sujal8889 <133516696+Sujal8889@users.noreply.github.com>
2023-05-13 21:37:49 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,19 @@
+var myDetails = {
+    firstName : "Rudra",
+    lastName : "Solanki",
+    email : "solankirudra556@gmail.com",
+    moNumber : 9106558832,
+};
+
+// console.log(myDetails.email);
+// console.log(myDetails["email"]);
+
+myDetails.email = "goblinbhai096@gmail.com";
+// console.log(myDetails.email);
+
+// console.log(myDetails.moNumber);
+
+// console.log(myDetails);
+
+console.table(myDetails);
+
