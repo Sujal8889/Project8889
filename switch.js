00b769083e8339779ef2b6f24912bc537588e2b4
Sujal8889 <133516696+Sujal8889@users.noreply.github.com>
2023-05-13 21:40:53 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,21 @@
+var std = 12;
+switch (std) {
+    case 8:
+        console.log("your sub: maths ss guj hindi science sanskrit");
+        break;
+    case 9:
+        console.log("your sub: maths ss hindi science sanskrit");
+        break;
+    case 10:
+        console.log("your sub: maths ss guj science sanskrit");
+        break;
+    case 11:
+        console.log("your sub: maths phy che comp eng");
+        break;
+    case 12:
+        console.log("your sub: maths phy che");
+        break;
+
+    default:
+        break;
+}
