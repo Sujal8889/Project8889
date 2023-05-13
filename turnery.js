14a25f99633661895d3efba8a25452be8c2ab1a0
Sujal8889 <133516696+Sujal8889@users.noreply.github.com>
2023-05-13 21:41:54 +0530

    Add files via upload

---
+++ turnery.js
@@ -0,0 +1,11 @@
+var logged = true;
+
+// if(logged){
+//     console.log("you are already logged in");
+// }
+// else{
+//     console.log("you have to login now");
+// }
+
+//turnery operator
+logged ? console.log("you are logged in already") : console.log("you have to login at now");
