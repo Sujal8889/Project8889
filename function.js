4aaff34af43b2705d026ea9c2ca0a90ddb82593c
Sujal8889 <133516696+Sujal8889@users.noreply.github.com>
2023-05-13 21:36:54 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,17 @@
+function sayHello(name) {
+      console.log("Hello, Mr."+name);  
+        console.log("How are you");
+        console.log(`Nice to meet you ${name}.coffee?`);
+}
+// sayHello();
+sayHello("Rudra");
+
+//deffrent between console and return
+
+function sayHii() {
+    return "Hii"
+}
+console.log(sayHii());
+//another way
+ var a = sayHii();
+ console.log(a);
